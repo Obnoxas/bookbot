5a7e35603b36c44610a3d890ebabfe9392a7b4be
Obnoxas <saxschulz@gmail.com>
2025-06-17 16:24:28 -0500

    removed hardcoded path, main now accepts cli inputs

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
 def main():
 
+    import sys
+
     from stats import count_words, count_characters, sort_dictionaries
 
     def get_book_text():
         book_text = f.read()
         return book_text
     
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
+    if len(sys.argv) == 2:
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {sys.argv[1]}")
+        print("----------- Word Count ----------")
 
-    with open("books/frankenstein.txt") as f:
-        count_words(get_book_text())
-    
-    print("--------- Character Count -------")
+        with open(sys.argv[1]) as f:
+            count_words(get_book_text())
+        
+        print("--------- Character Count -------")
 
-    with open("books/frankenstein.txt") as f:
-        count_characters(get_book_text())
+        with open(sys.argv[1]) as f:
+            count_characters(get_book_text())
 
-    with open("books/frankenstein.txt") as f:
-        sorted_dictionaries = sort_dictionaries(count_characters(get_book_text()))
+        with open(sys.argv[1]) as f:
+            sorted_dictionaries = sort_dictionaries(count_characters(get_book_text()))
 
-    for character in sorted_dictionaries:
-        if str.isalpha(character["char"]) is True:
-            print(f"{character["char"]}: {character["num"]}")
+        for character in sorted_dictionaries:
+            if str.isalpha(character["char"]) is True:
+                print(f"{character["char"]}: {character["num"]}")
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
 main()
